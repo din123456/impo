4a5d7bc2c2c11ca39d7fa094ad7342430fc0dccc
din123456 <dineshsadhu2000@gmail.com>
2022-09-27 11:24:07 +0530

    MY SECOND COMMIT

--- src/main/java/com/infy/DemoSpringRestApplication.java
+++ src/main/java/com/infy/DemoSpringRestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoSpringRestApplication {
 
 	public static void main(String[] args) {
-		//dinesh kumar
+		//fully satis fieaalklsdnfn;ksdf;oasdfnaso;oo
 		SpringApplication.run(DemoSpringRestApplication.class, args);
 	}
 
