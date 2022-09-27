8f4841c91c70923880e21141ccc45f8b3d87a6df
din123456 <dineshsadhu2000@gmail.com>
2022-09-27 11:03:33 +0530

    third commit

--- src/main/java/com/infy/DemoSpringRestApplication.java
+++ src/main/java/com/infy/DemoSpringRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoSpringRestApplication {
 
 	public static void main(String[] args) {
+		//dinesh kumar
 		SpringApplication.run(DemoSpringRestApplication.class, args);
 	}
 
